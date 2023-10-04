eae0d5634981046ec87056124e258e7cdc38b76c
Ping-Sung <76198720+Ping-Sung@users.noreply.github.com>
2023-10-04 17:44:48 -0400

    update gnicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 
 bind = "0.0.0.0:8000"  # 绑定的地址和端口
-workers = 4  # 工作进程的数量
+workers = 1  # 工作进程的数量
 timeout = 120  # 请求超时时间（以秒为单位）
 errorlog = os.path.join(os.path.dirname(BASE_DIR),
                         'logs/error.log')  # 错误日志文件的路径
